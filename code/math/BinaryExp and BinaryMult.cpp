f4bf9e06c0e73572de43500b86377c6e31a51984
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2020-02-02 20:39:19 -0300

    Create BinaryExp and BinaryMult.cpp

---
+++ code/math/BinaryExp and BinaryMult.cpp
@@ -0,0 +1,27 @@
+
+//returns base^exp %mod
+ll BinExp(ll base, ll exp, ll mod){
+	ll ans =1;
+	while(exp){
+		if(exp & 1){
+			ans=BinMult(ans,base,mod);
+		}
+		base=BinMult(base,base,mod);
+		exp>>=1;
+	}
+	return ans;
+}
+
+//returns a*b %mod
+ll BinMult(ll a, ll b, ll mod){
+	ll ans=0;
+	a%=mod;
+	while(b){
+		if(b & 1){
+			ans=(ans+a)%mod;
+		}
+		a=(a*2)%mod;
+		b>>=1;
+	}
+	return ans;
+}
