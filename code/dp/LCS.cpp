cce62a45d99857a1ba0d64b6a4679378717d050f
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2020-02-19 22:26:06 -0300

    Create LCS.cpp

---
+++ code/dp/LCS.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lcs[3030][3030]; 
+//lcs[i][j] lcs from s[0...i-1] to t[0...j-1]
+//in here strings are one based
+string s,t;
+
+int main(){
+	memset(lcs,0,sizeof(lcs));
+	cin>>s>>t;
+	for(int i=1;i<=s.size();i++){
+		for(int j=1;j<=t.size();j++){
+			if(s[i-1]==t[j-1])
+				lcs[i][j]=1+lcs[i-1][j-1];
+			else
+				lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1]);
+
+		}
+	}
+
+	//retrieving the lcs
+	int l,r;
+	l=s.size(),r=t.size();
+	string ret="";
+	while(lcs[l][r]>0){
+		while(lcs[l-1][r]==lcs[l][r])l--;
+		
+		while(lcs[l][r-1]==lcs[l][r])r--;
+
+		ret+=t[r-1];
+		l--;
+		r--;
+	}
+	reverse(ret.begin(),ret.end());
+	cout<<ret<<"\n";
+
+    return 0;
+}
