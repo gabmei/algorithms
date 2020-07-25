8b9936cb98878ae72d7b1f94d1d416ffc099ff78
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2020-07-25 15:51:01 -0300

    Create geometric-sum.cpp
    Added recursive and iterative implementation

---
+++ code/math/geometric-sum.cpp
@@ -0,0 +1,37 @@
+//S(k) = I + A + A^2 + ... + A^k-1
+/*----------------------------------------------------------*/
+//Recursive geometric sum implementation
+template<class T>
+Matrix<T> GP_sum(Matrix<T> &ratio, ll n){
+	Matrix<T> ret;
+	if(n==0)return ret;
+	Matrix<T> I,A;
+	I.identity();
+	A=ratio;
+	A.binExp(n/2);
+	if(n%2==1)ret=A,A=A*ratio;
+
+	return ret=ret + (I+A)*PG_sum(ratio,n/2);
+}
+/*----------------------------------------------------------*/
+//Iterative geometric sum implementation
+template<class T>
+Matrix<T> GP_sum(Matrix<T> &ratio, ll n){
+
+	Matrix<T> ans,I,ans_pot,magic;
+	I.identity();
+	magic=I;
+	ans_pot=I;
+	while(n>0){
+		if(n&1){
+			ans = ans_pot*magic + ans;
+			ans_pot=ans_pot*ratio;
+		}
+		magic = ratio*magic + magic;
+		ratio=ratio*ratio;
+		n/=2;
+	}
+
+	return ans;
+}
+/*-----------------------------------------------*/
