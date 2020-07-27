22f4174416bfdd9eae5aed48ec82275a85b1409d
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2020-07-27 13:20:57 -0300

    Update num_div.cpp

--- code/math/num_div.cpp
+++ code/math/num_div.cpp
@@ -1,10 +1,26 @@
+//get primes up to sqrt(n)
+bool is_prime[lim];
+vector<int> sieve(int n){
+	memset(is_prime,1,sizeof(is_prime));
+	is_prime[0]=is_prime[1]=false;
+	vector<int> primes;
+	for(int i=2;i*i<n;i++){
+		if(is_prime[i]){
+			primes.push_back(i);
+			if((ll)i*i > n)continue;
+			for(int j=i*i;j<n;j+=i){
+				is_prime[j]=false;
+			}
+		}
+	}
+	return primes;
+}
+ 
 ll num_div(int n,vector<int> &primes){
-	if(n==1)return 1;
-	if(binary_search(primes.begin(),primes.end(),n))return 2;
-
 	ll ret=1;
+	int m=n;
 	for(int p:primes){
-		if(n==1)break;
+		if((ll)p*p>m)break;
 		if(n%p==0){
 			int exp=0;
 			while(n%p==0){
@@ -14,6 +30,7 @@ ll num_div(int n,vector<int> &primes){
 			ret*=(exp+1);
 		}
 	}
-
+	if(n>1)ret*=2;// n is prime
+ 
 	return ret;
 }
