691643add501ea041aa9bb349421acb5589b43d9
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2020-10-28 15:05:26 -0300

    Update kruskal.cpp

--- code/data structures/dsu/kruskal.cpp
+++ code/data structures/dsu/kruskal.cpp
@@ -1,31 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int>p,sz;
-void init_(int n){
-	p=vector<int>(n+1);
-	iota(p.begin(),p.end(),0);
+struct DSU{
+    vector<int>p,sz;
+    DSU(int n):p(n),sz(n){//0-based
+        iota(p.begin(),p.end(),0);
+        fill(sz.begin(),sz.end(),1);
+    }
+    int getSet(int a){
+        return p[a]=(p[a]==a?a:getSet(p[a]));//path compression
+    }
+    void unionSet(int a, int b){
+        a=getSet(a);
+        b=getSet(b);
+ 
+        if(a!=b){
+            //Union by size
+            if(sz[a]>sz[b]){
+                swap(a,b);
+            }
+            p[a]=b;
+            sz[b]+=sz[a];
+        }
+    }
+    int countSets(){
+    	int n = (int)p.size(),cnt=0;
+    	for(int i=0;i<n;i++){
+    		cnt+=i==getSet(i);
+    	}
+    	return cnt;
+    }
+};
 
-	sz=vector<int>(n+1,1);
-}
-int get_set(int a){
-	return p[a]=(p[a]==a?a:get_set(p[a]));//path compression
-}
-
-void union_set(int a, int b){
-	a=get_set(a);
-	b=get_set(b);
-
-	if(a!=b){
-		//Union by size
-		if(sz[a]>sz[b]){
-			swap(a,b);
-		}
-		p[a]=b;
-		sz[b]+=sz[a];
-
-	}
-}
 struct Edge{
 	int u,v,w;
 	Edge(int u,int v, int w):u(u),v(v),w(w){}
@@ -34,27 +40,26 @@ struct Edge{
 	}
 };
 
-vector<Edge>edges;
-
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int n,m;
 	cin>>n>>m;
-	init_(n);
-
+	DSU dsu(n);
+	vector<Edge>edges;
 	for(int i=0;i<m;i++){
 		int u,v,w;
 		cin>>u>>v>>w;
+		u--;v--;
 		edges.emplace_back(u,v,w);
 	}
 	sort(edges.begin(),edges.end());
 	long long total_cost=0;
 	for(Edge e:edges){
-		if(get_set(e.u)!=get_set(e.v)){//get edges with lowest values
+		if(dsu.getSet(e.u)!=dsu.getSet(e.v)){//get edges with lowest values
 			total_cost+=e.w;
-			union_set(e.u,e.v);
+			dsu.unionSet(e.u,e.v);
 		}
 	}
 	cout<<total_cost<<'\n';
