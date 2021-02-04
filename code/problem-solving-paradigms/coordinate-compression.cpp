e4d949d8e5b2c908823d95c5cd68ee86015be7be
Gabriel Meireles <48731050+gabsho@users.noreply.github.com>
2021-02-04 19:27:57 -0300

    Update coordinate-compression.cpp

--- code/problem-solving-paradigms/coordinate-compression.cpp
+++ code/problem-solving-paradigms/coordinate-compression.cpp
@@ -7,12 +7,12 @@ struct CoordinateCompression{
         v.erase(unique(v.begin(),v.end()),v.end());
         return (int)v.size();
     }
-    int operator[](const T &a){ //first position greater or equal to a
+    int operator[](const T &a){
         auto it=lower_bound(v.begin(),v.end(),a);
-        return int( it-v.begin() );
+        return it != v.end() && *it == a ? int(it - v.begin()):-1;
     }
-    bool count(const T &a){
+    int lower_bound(const T &a){
         auto it=lower_bound(v.begin(),v.end(),a);
-        return it != v.end() && *it == a;
+        return int(it - v.begin());
     }
 };
