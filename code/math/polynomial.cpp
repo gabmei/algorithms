9c735b96d1c6b3f72e042ed0c2a046573a576112
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-02-16 22:29:34 -0300

    Create polynomial.cpp

---
+++ code/math/polynomial.cpp
@@ -0,0 +1,29 @@
+template<class T>
+struct vec{
+    int n;
+    vector<T>v;
+    vec(int n_,T val=0):n(n_),v(n,val){}
+    void operator *=(const vec& o){ *this = *this * o; }
+    vec operator *(const vec& o){
+        vec r(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int id=(i+j);
+                if(id>=n)id-=n;
+                r.v[id]+=v[i]*o.v[j];
+            }
+        }
+        return r;
+    }
+    T& operator[](int i){ return v[i]; }
+};
+template<class T>
+vec<T> binExp(vec<T>a,long long e){
+    vec<T>r(a.n);r.v[0]=1;
+    while(e){
+        if(e&1)r*=a;
+        a*=a;   
+        e>>=1;
+    }
+    return r;
+}
