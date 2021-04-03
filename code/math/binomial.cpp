056147eb5fe534a80d332ee24fed9f0537f62cee
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-04-03 15:02:20 -0300

    Update binomial.cpp

--- code/math/binomial.cpp
+++ code/math/binomial.cpp
@@ -1,64 +1,30 @@
-typedef long long ll;
-const ll mod = 1e9+7;
-vector<ll>fat,fat_inv,inv; 
-/*----------------------------------------------------------*/
-//fat_inv[k] = modular inverse of k! mod m
-//inv[k] = modular inverse of k mod m
-//fat_inv[k] = fat_inv[k-1]*inv[k] mod m
-
-//This implementation only works if m is prime
-
-//Precalculates all inverses in O(n)
-void fill_fat(int n){
-	fat=fat_inv=inv=vector<ll>(n+1);
-	for(int i=0;i<2;i++){
-		fat[i]=fat_inv[i]=1;
-	}
-	inv[1]=1;
-
-	for(ll i=2;i<=n;i++){
-		fat[i]=fat[i-1]*i %mod;
-		inv[i]=mod -(mod/i)*inv[mod%i]%mod;
-		fat_inv[i]=fat_inv[i-1]*inv[i]%mod;
-	}
-}
-
-ll comb(ll n, ll k){
-	ll ret = fat[n]*fat_inv[k]%mod;
-	return ret = ret*fat_inv[n-k]%mod;
-}
-
-
-/*----------------------------------------------------------*/
-//a^(phi(m)-1)=a^-1 mod m
-//a^(m-2)=a^-1 mod m if m prime
-void fill_fat(int n){
-	fat=vector<ll>(n+1,1);
-	fat_inv=vector<ll>(n+1,-1);
-	for(ll i=2;i<=n;i++){
-		fat[i]=fat[i-1]*i %mod;
-	}
-}
-
-ll binExp(ll b, ll e){
-	ll ret=1;
-	while(e){
-		if(e&1){
-			ret=ret*b %mod;
-		}
-		b=b*b %mod;
-		e/=2;
-	}
-	return ret;
-}
-
-ll comb(ll n, ll k){
-	if(fat_inv[k]==-1)fat_inv[k]=binExp(fat[k],mod-2);
-	if(fat_inv[n-k]==-1)fat_inv[n-k]=binExp(fat[n-k],mod-2);
- 
-	ll ret = fat[n]*fat_inv[k]%mod;
-	return ret = ret*fat_inv[n-k]%mod;
-}
-
-
-
+const int mod=1e9+7;
+struct MB{
+    int x;
+    MB(int x_=0):x(x_){}
+    void operator +=(const MB& o){ *this = *this + o; }
+    void operator -=(const MB& o){ *this = *this - o; }
+    void operator *=(const MB& o){ *this = *this * o; }
+    MB& operator ++(){ return *this = *this + MB(1); }
+    MB operator ++(int){ MB r=*this; ++(*this); return r; }
+    MB operator +(const MB& o){ return x+o.x >=mod ? x+o.x-mod : x+o.x; }
+    MB operator -(const MB& o){ return x-o.x < 0 ? x-o.x+mod : x-o.x; }
+    MB operator *(const MB& o){ return int(1ll*x*o.x%mod); }
+    operator int(){return x;}
+    friend ostream& operator<< (ostream& os, MB& o) { return os << o.x; }
+    friend istream& operator>> (istream& is, MB& o) { return is >> o.x; }
+};
+struct Binomial{
+    vector<MB>fat,invFat,inv;
+    Binomial(int n):fat(n,1),invFat(n,1),inv(n,0){
+        inv[1] = 1;
+        for(int i=2;i<n;++i){
+            fat[i] = MB(i)*fat[i-1];
+            inv[i] = MB(mod) - MB(mod/i)*inv[mod%i]; 
+            invFat[i] = invFat[i-1]*inv[i];
+        }
+    }
+    MB comb(int n, int k){
+        return fat[n]*invFat[k]*invFat[n-k];
+    }
+};
