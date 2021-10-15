4b02b80baaa64c8117fb6acd7e23d4272678f0f6
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-10-15 20:09:58 -0300

    Update heavy-light-decomposition.cpp

--- code/graph/heavy-light-decomposition.cpp
+++ code/graph/heavy-light-decomposition.cpp
@@ -1,53 +1,73 @@
-vector<vector<int>> adj;
-struct HeavyLight{
-	vector<int>pos,pai,h,sz;
-	//vector<int> v; //vetor para buildar a segtree
-	SegmentTree tree;
-	int timer;
+//IS_EDGE: whether queries are on vertices or edges
+//false: vertices, true: edges
+template<const bool IS_EDGE>
+struct HLD {
+	vector<int> tin, tout, sz, rin, p, nxt, h;
+	vector<vector<int>> adj;
+	int t;
+	HLD(int n):tin(n),tout(n),sz(n),rin(n),p(n),nxt(n),h(n),adj(n){}
+
+	void addEdge(int u, int v) {
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+
+	void setRoot(int n) {
+		t = 0;
+		p[n] = n;
+		h[n] = 0;
+		prep(n, n);
+		nxt[n] = n;
+		hld(n,n);
+	}
+
+	int getLCA(int u, int v) {
+		while(!inSubtree(nxt[u], v)) u = p[nxt[u]];
+		while(!inSubtree(nxt[v], u)) v = p[nxt[v]];
+		return tin[u] < tin[v] ? u : v;
+	}
+
+	bool inSubtree(int u, int v) {
+		// is v tin the subtree of u
+		return tin[u] <= tin[v] && tin[v] < tout[u];
+	}
+
+	vector<pair<int, int>> getPathtoAncestor(int u, int anc) {
+		// returns ranges [l, r) that the path has
+		vector<pair<int, int>> ans;
 	
-	HeavyLight(int n, int root=0):pos(n),pai(n),h(n),sz(n),tree(n){	
-		dfs(root);
-		timer = 0;
-		h[root]=root;
-		buildHLD(root);
-		//tree.build(v);
+		while(nxt[u] != nxt[anc]) {
+			ans.emplace_back(tin[nxt[u]], tin[u] + 1);
+			u = p[nxt[u]];
+		}
+		// this includes the ancestor!
+		// check if range [l,r) is valid when IS_EDGE
+		ans.emplace_back(tin[anc] + IS_EDGE, tin[u] + 1);
+		return ans;
 	}
-	void dfs(int u, int p=-1){
+	void prep(int u, int par) {
 		sz[u] = 1;
-		for(int &v:adj[u]){
-			if(v!=p){
-				dfs(v,u);
-				sz[u]+=sz[v];
-				if(sz[v] > sz[adj[u][0]] || adj[u][0] == p)swap(v,adj[u][0]);
+		p[u] = par;
+		for(int& v : adj[u]) {
+			if(v != par){
+				h[v] = 1 + h[u];
+				prep(v, u);
+				sz[u] += sz[v];
+				if(sz[v] > sz[adj[u][0]] || adj[u][0] == v) {
+					swap(adj[u][0], v);
+				}
 			}
 		}
 	}
-	void buildHLD(int u, int p=-1){
-		pos[u]=timer++;
-		//v[pos[u]]=val[u]; // valor presente no nó u
-		for(int v:adj[u]){
-			if(v!=p){
-				pai[v]=u;
-				h[v] =(v==adj[u][0] ? h[u] : v);
-				buildHLD(v,u);
-			}
+
+	void hld(int u, int par) {
+		tin[u] = t++;
+		rin[tin[u]] = u;
+		for(auto v : adj[u]) {
+			if(v == par)continue;
+			nxt[v] = (v == adj[u][0] ? nxt[u] : v);
+			hld(v,u);
 		}
-	}
-	node queryPath(int a ,int b){
-		if(pos[a]<pos[b])swap(a,b);
- 
-		if(h[a]==h[b])return tree.query(pos[b],pos[a]);
- 
-		return tree.query(pos[h[a]],pos[a]) + queryPath(pai[h[a]],b);
-	}
-	void updatePath(int a, int b, node x){	
-		if(pos[a]<pos[b])swap(a,b);
- 
-		if(h[a]==h[b]){
-			tree.update(pos[b],pos[a],x);
-			return;
-		}
-		tree.update(pos[h[a]],pos[a],x);
-		updatePath(pai[h[a]],b,x);
+		tout[u] = t;
 	}
 };
