975a8e38bff66517db5ebb5b7e0fba1c33b5a4aa
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-10-15 21:33:19 -0300

    Update standard.cpp

--- code/data-structures/segment-tree/standard.cpp
+++ code/data-structures/segment-tree/standard.cpp
@@ -1,60 +1,63 @@
 struct node{
-	int x;
-	node(int _x=0):x(_x){}
+	int x; //atributes
+	node():x(0){} //empty constructor
+	node(int _x):x(_x){} // init
 	node operator+(const node &o){ // merge function
-		return node(x^o.x);
+		return node(max(x,o.x));
 	}
-}nil(0);
+};
+//T: internal type
+//F: external type
+template<class T, class F>
 struct SegmentTree{
 	int n;
-	vector<node>tree;
-	SegmentTree(int _n=0):n(_n),tree(4*n,nil){}
-	void build(int pos,int tl,int tr,const vector<node> &arr){
+	vector<T>tree;
+	SegmentTree(int _n=0):n(_n),tree(4*n){}
+	void build(int pos,int tl,int tr,const vector<F> &arr){
 		int esq=2*pos, dir=2*pos+1;
 		int mid = (tl+tr)/2;
 		if(tl==tr){
-			tree[pos]=arr[tl];
+			tree[pos] = T(arr[tl]);
 		}else{
 			build(esq,tl,mid,arr);
 			build(dir,mid+1,tr,arr);
  
-			tree[pos]=tree[esq]+tree[dir];
+			tree[pos] = tree[esq] + tree[dir];
 		}
 	}
-	void build(const vector<node> &arr){
+	void build(const vector<F> &arr){
 		build(1,0,n-1,arr);
 	}
-	
-	void update(int pos,int tl,int tr,int x, node val){
+	void update(int pos,int tl,int tr,int x, T val){
 		int esq=2*pos, dir=2*pos+1;
 		int mid = (tl+tr)/2;
 		if(tl==tr){
-			tree[pos]=tree[pos]+val;//assign update
+			tree[pos] = val;
 		}else{
 			if(x<=mid)update(esq,tl,mid,x,val);
 			else update(dir,mid+1,tr,x,val);
 	 
-			tree[pos]=tree[esq]+tree[dir];
+			tree[pos] = tree[esq] + tree[dir];
 		}
 	}
  
-	void update(int x, node val){
-		update(1,0,n-1,x,val);
+	void update(int x, F val){
+		update(1,0,n-1,x,T(val));
 	}
  
-	node query(int pos, int tl, int tr, int l, int r){
+	T query(int pos, int tl, int tr, int l, int r){
 		int esq=2*pos, dir=2*pos+1;
 		int mid = (tl+tr)/2;
 	 
 		if(tl>r || tr<l){
-			return nil;//depends of the query type
+			return T(); 
 		}else if(tl>=l && tr<=r){
 			return tree[pos];
 		}else{
-			return query(esq,tl,mid,l,r)+query(dir,mid+1,tr,l,r);
+			return query(esq,tl,mid,l,r) + query(dir,mid+1,tr,l,r);
 		}
 	}
-	node query(int l, int r){
+	T query(int l, int r){
 		return query(1,0,n-1,l,r);
 	}
 };
