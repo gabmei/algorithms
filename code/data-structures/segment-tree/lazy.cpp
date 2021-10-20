4b14ac5b7ed9a0a9e84fe59e3b65c507844e9b87
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-10-20 15:45:12 -0300

    Update lazy.cpp

--- code/data-structures/segment-tree/lazy.cpp
+++ code/data-structures/segment-tree/lazy.cpp
@@ -1,82 +1,92 @@
-struct node{
-    int x;
-    node(int _x=0):x(_x){}
-	node operator+(const node& o){ // merge function
-		return node(x+o.x);
-	}
-	bool operator!=(const node& o){
-		return o.x != x;
-	}
-}nil(0),nada(-1);
- 
-struct SegmentTree{
-	int n;
-	vector<node>tree,lazy;
-	SegmentTree(int _n=0):n(_n),tree(4*n,nil),lazy(4*n,nada){}
+struct LazyContext{
+	int x; //atributes
+	LazyContext():x(0){} //empty constructor
+	LazyContext(int _x):x(_x){} // init
+	void operator+=(const LazyContext& o){ // update Lazy
 
-	void calc(node& a, const node& b, int len){
-		a= node(b.x*len);
 	}
-    
-	void push(int pos, int tl,int tr){ //assignment update, sum query
-		if(lazy[pos]!=nada){
-			int esq=2*pos, dir=2*pos+1;
-			int mid = (tl+tr)/2;
-			calc(tree[esq],lazy[pos],mid-tl+1);
-			calc(tree[dir],lazy[pos],tr-(mid+1)+1);
+	bool empty(){ return x == 0; }
+	void reset(){ x = 0; }
+
+};
+struct Node{
+	int x; //atributes
+	Node():x(0){} //empty constructor
+	Node(int _x):x(_x){} // init
+	Node operator+(const Node& o){ // merge function
 		
-			lazy[esq]=lazy[dir]=lazy[pos];
-			lazy[pos]=nada;
-		}
 	}
-	void build(int pos,int tl,int tr, const vector<node>& arr){
-		int esq=2*pos, dir=2*pos+1;
-		int mid = (tl+tr)/2;
-		if(tl==tr){
-			tree[pos]=arr[tl];
+	void operator+=(const LazyContext& l){ //update Node with Lazy
+		
+	}
+};
+//T: internal type
+//F: external type
+//L: Lazy type
+template<class T, class F, class L>
+struct SegmentTree{
+public:
+	SegmentTree(int _n=0):n(_n),tree(4*n),lazy(4*n){}
+	void build(const vector<F>& arr){
+		build(1, 0, n-1, arr);
+	}
+	void update(int l, int r, L val){
+		update(1, 0, n-1, l, r, val);
+	}
+	T query(int l, int r){
+		T cur;
+		query(1, 0, n-1, l, r, cur);
+		return cur;
+	}
+private:
+	int n;
+	vector<T>tree;
+	vector<L>lazy;
+	void build(int pos, int tl, int tr, const vector<F>& arr){
+		if(tl == tr){
+			tree[pos] = T(arr[tl]);
 		}else{
-			build(esq,tl,mid,arr);
-			build(dir,mid+1,tr,arr);
- 
-			tree[pos]=tree[esq] + tree[dir];
+			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
+			build(esq, tl, mid, arr);
+			build(dir, mid+1, tr, arr);
+
+			tree[pos] = tree[esq] + tree[dir];
 		}
 	}
-	void build(const vector<node>& arr){
-		build(1,0,n-1,arr);
-	}
-	void update(int pos,int tl,int tr,int l, int r, node x){
-		int esq=2*pos, dir=2*pos+1;
-		int mid = (tl+tr)/2;
+	void update(int pos, int tl, int tr, int l, int r, const L& val){
 		if(tl>r || tr<l){
 			return;
 		}else if(tl>=l && tr<=r){
-			calc(tree[pos],x,tr-tl+1);
-		    	lazy[pos]=x;
+			tree[pos] += val;
+			lazy[pos] += val;
 		}else{
-			push(pos,tl,tr);
-			update(esq,tl,mid,l,r,x);
-			update(dir,mid+1,tr,l,r,x);
- 
-			tree[pos]=tree[esq]+tree[dir];
+			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
+			apply(pos);
+			update(esq, tl, mid, l, r, val);
+			update(dir, mid+1, tr, l, r, val);
+
+			tree[pos] = tree[esq] + tree[dir];
 		}
 	}
- 
-	void update(int l, int r, node x){
-		update(1,0,n-1,l,r, x);
-	}
-	node query(int pos, int tl, int tr, int l, int r){
-		int esq=2*pos, dir=2*pos+1;
-		int mid = (tl+tr)/2;
+	void query(int pos, int tl, int tr, int l, int r, T& cur){
 		if(tl>r || tr<l){
-			return nil;//depends of the query type
+			return;
 		}else if(tl>=l && tr<=r){
-			return tree[pos];
+			cur = cur + tree[pos];
 		}else{
-			push(pos,tl,tr);
-			return query(esq,tl,mid,l,r) + query(dir,mid+1,tr,l,r);
+			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
+			apply(pos);
+			query(esq,tl,mid,l,r,cur);
+			query(dir,mid+1,tr,l,r,cur);
 		}
 	}
-	node query(int l, int r){
-		return query(1,0,n-1,l,r);
+	void apply(int pos){
+		if(!lazy[pos].empty()){
+			int esq = 2 * pos, dir = esq + 1;
+			tree[esq] += lazy[pos];tree[dir] += lazy[pos];
+			lazy[esq] += lazy[pos];lazy[dir] += lazy[pos];
+
+			lazy[pos].reset();
+		}
 	}
 };
