9e42863362c77ca47027b206491f77174d754bb2
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-10-22 01:06:34 -0300

    Update standard.cpp

--- code/data-structures/segment-tree/standard.cpp
+++ code/data-structures/segment-tree/standard.cpp
@@ -6,29 +6,27 @@ struct Node{
 		
 	}
 };
-//T: internal type
-//F: external type
-template<class T, class F>
+template<class T>
 struct SegmentTree{
 public:
 	SegmentTree(int _n=0):n(_n),tree(4*n){}
-	void build(const vector<F>& arr){
+	SegmentTree(const vector<T>& arr):n((int)arr.size()),tree(4*n){
 		build(1, 0, n-1, arr);
 	}
-	void update(int x, F val){
-		update(1, 0, n-1, x, T(val));
+	void update(int x, T val){
+		update(1, 0, n-1, x, val);
 	}
 	T query(int l, int r){
-		T cur;
+		T cur = T();
 		query(1, 0, n-1, l, r, cur);
 		return cur;
 	}
 private:
 	int n;
 	vector<T>tree;
-	void build(int pos, int tl, int tr, const vector<F>& arr){
+	void build(int pos, int tl, int tr, const vector<T>& arr){
 		if(tl == tr){
-			tree[pos] = T(arr[tl]);
+			tree[pos] = arr[tl];
 		}else{
 			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
 			build(esq, tl, mid, arr);
@@ -37,6 +35,29 @@ private:
 			tree[pos] = tree[esq] + tree[dir];
 		}
 	}
+	void update(int pos, int tl, int tr, int x, const T& val){
+		if(tl==tr){
+			tree[pos] = val;
+		}else{
+			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
+			if(x<=mid) update(esq, tl, mid, x, val);
+			else update(dir, mid+1, tr, x, val);
+	
+			tree[pos] = tree[esq] + tree[dir];
+		}
+	}
+	void query(int pos, int tl, int tr, int l, int r, T& cur){
+		if(tl>r || tr<l){
+			return;
+		}else if(tl>=l && tr<=r){
+			cur = cur + tree[pos];
+		}else{
+			int mid = (tl + tr)/2, esq = 2 * pos, dir = esq + 1;
+			query(esq, tl, mid, l, r, cur);
+			query(dir, mid+1, tr, l, r, cur);
+		}
+	}
+};}
 	void update(int pos, int tl, int tr, int x, const T& val){
 		if(tl==tr){
 			tree[pos] = val;
