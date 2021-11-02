b2d6bac8912f5e76db5b175b7869c995cbba3f8f
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-11-02 20:08:09 -0300

    Update modular-arithmetic.cpp

--- code/math/modular-arithmetic.cpp
+++ code/math/modular-arithmetic.cpp
@@ -1,4 +1,4 @@
-const int mod=1e9+7;
+const int MOD=1e9+7;
 template<class T>
 T binExp(T a, long long e){
     T r(1);
@@ -17,10 +17,10 @@ struct MB{
     void operator /=(const MB& o){ *this = *this / o; }
     MB& operator ++(){ return *this = *this + MB(1); }
     MB operator ++(int){ MB r=*this; ++(*this); return r; }
-    MB operator +(const MB& o){ return x+o.x >=mod ? x+o.x-mod : x+o.x; }
-    MB operator -(const MB& o){ return x-o.x < 0 ? x-o.x+mod : x-o.x; }
-    MB operator *(const MB& o){ return int(1ll*x*o.x%mod); }
-    MB operator /(const MB& o){ return *this * binExp(o,mod-2); }
+    MB operator +(const MB& o){ return x+o.x >=MOD ? x+o.x-MOD : x+o.x; }
+    MB operator -(const MB& o){ return x-o.x < 0 ? x-o.x+MOD : x-o.x; }
+    MB operator *(const MB& o){ return int(1ll*x*o.x%MOD); }
+    MB operator /(const MB& o){ return *this * binExp(o,MOD-2); }
     operator int(){return x;}
     friend ostream& operator<< (ostream& os, MB& o) { return os << o.x; }
     friend istream& operator>> (istream& is, MB& o) { return is >> o.x; }
