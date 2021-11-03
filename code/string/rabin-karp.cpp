f7b55063e1ebb9859d77b7abcad994e9fdb718ff
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-11-03 14:14:59 -0300

    Update rabin-karp.cpp

--- code/string/rabin-karp.cpp
+++ code/string/rabin-karp.cpp
@@ -2,8 +2,8 @@ template<class F = unsigned int, class T = string>
 struct RabinKarp{
     int n;
     vector< F > p, pw;
-	static constexpr F C[2] = {727,137};
-	RabinKarp(){}
+    static constexpr F C[2] = {727,137};
+    RabinKarp(){}
     //change C id for double hashing
     RabinKarp(const T& s, bool id):n((int)s.size()),p(n+1),pw(n+1,1){
         for(int i=1;i<=n;i++){
