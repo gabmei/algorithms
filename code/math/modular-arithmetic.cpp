335066dda21d4b8b19536fdbdbb4e7e0bd743d3e
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-11-07 01:45:07 -0300

    refactor operators

--- code/math/modular-arithmetic.cpp
+++ code/math/modular-arithmetic.cpp
@@ -11,17 +11,33 @@ T binExp(T a, long long e){
 struct MB{
     int x;
     constexpr MB(int _x=0):x(_x >= MOD ? _x - MOD : _x < 0 ? _x + MOD : _x){}
-    void operator +=(const MB& o){ *this = *this + o; }
-    void operator -=(const MB& o){ *this = *this - o; }
-    void operator *=(const MB& o){ *this = *this * o; }
-    void operator /=(const MB& o){ *this = *this / o; }
-    MB& operator ++(){ return *this = *this + MB(1); }
-    MB operator ++(int){ MB r=*this; ++(*this); return r; }
-    MB operator +(const MB& o){ return x+o.x >=MOD ? x+o.x-MOD : x+o.x; }
-    MB operator -(const MB& o){ return x-o.x < 0 ? x-o.x+MOD : x-o.x; }
-    MB operator *(const MB& o){ return int(1ll*x*o.x%MOD); }
-    MB operator /(const MB& o){ return *this * binExp(o,MOD-2); }
-    explicit operator int(){return x;}
+    MB& operator+=(const MB& rhs){
+        x += rhs.x;
+        if(x >= MOD) x -= MOD;
+        return *this;
+    }
+    MB& operator-=(const MB& rhs){
+        x -= rhs.x;
+        if(x < 0) x += MOD;
+        return *this; 
+    }
+    MB& operator*=(const MB& rhs){
+        x = int(1ll * x * rhs.x % MOD);
+        return *this;
+    }
+    MB& operator/=(const MB& rhs){
+        return *this *= binExp(rhs, MOD - 2);
+    }
+    friend MB operator+(MB lhs, const MB& rhs){ return lhs += rhs; }
+    friend MB operator-(MB lhs, const MB& rhs){ return lhs -= rhs; }
+    friend MB operator*(MB lhs, const MB& rhs){ return lhs *= rhs; }
+    friend MB operator/(MB lhs, const MB& rhs){ return lhs /= rhs; }
+
+    MB operator-() const { return MB() - *this; }
+    bool operator==(const MB& rhs) const { return x == rhs.x; }
+    bool operator!=(const MB& rhs) const { return !(*this == rhs); }
+
+    
     friend ostream& operator<< (ostream& os, MB o) { return os << o.x; }
     friend istream& operator>> (istream& is, MB& o) { return is >> o.x; }
 };
