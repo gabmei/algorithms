7f51469a3267e65811348b52a0a5ca9515bd5d0c
Gabriel Meireles <gasm@cin.ufpe.br>
2021-11-07 21:37:00 -0300

    Fix indentation

--- code/data-structures/segment-tree/lazy.cpp
+++ code/data-structures/segment-tree/lazy.cpp
@@ -40,58 +40,58 @@ public:
 		return cur;
 	}
 private:
-	int n;
-	vector<T>tree;
-	vector<L>lazy;
+    int n;
+    vector<T>tree;
+    vector<L>lazy;
     static int left (int p){ return 2 * p; }
     static int right (int p){ return 2 * p + 1; }
-	void build(int p, int tl, int tr, const vector<T>& arr){
-		if(tl + 1 == tr){
-			tree[p] = arr[tl];
-		}else{
-			int mid = (tl + tr)/2;
-			build(left(p), tl, mid, arr);
-			build(right(p), mid, tr, arr);
+    void build(int p, int tl, int tr, const vector<T>& arr){
+        if(tl + 1 == tr){
+            tree[p] = arr[tl];
+        }else{
+            int mid = (tl + tr)/2;
+            build(left(p), tl, mid, arr);
+            build(right(p), mid, tr, arr);
 
-			tree[p] = tree[left(p)] + tree[right(p)];
-		}
-	}
-	void update(int p, int tl, int tr, int l, int r, const L& val){
-		if(tl>=r || tr<=l){
-			return;
-		}else if(tl>=l && tr<=r){
-			tree[p].apply(val);
-			lazy[p] += val;
-		}else{
-			int mid = (tl + tr)/2;
-			apply(p);
-			update(left(p), tl, mid, l, r, val);
-			update(right(p), mid, tr, l, r, val);
+            tree[p] = tree[left(p)] + tree[right(p)];
+        }
+    }
+    void update(int p, int tl, int tr, int l, int r, const L& val){
+        if(tl>=r || tr<=l){
+            return;
+        }else if(tl>=l && tr<=r){
+            tree[p].apply(val);
+            lazy[p] += val;
+        }else{
+            int mid = (tl + tr)/2;
+            apply(p);
+            update(left(p), tl, mid, l, r, val);
+            update(right(p), mid, tr, l, r, val);
 
-			tree[p] = tree[left(p)] + tree[right(p)];
-		}
-	}
-	void query(int p, int tl, int tr, int l, int r, T& cur){
-		if(tl>=r || tr<=l){
-			return;
-		}else if(tl>=l && tr<=r){
-			cur += tree[p];
-		}else{
-			int mid = (tl + tr)/2;
-			apply(p);
-			query(left(p), tl, mid, l, r, cur);
-			query(right(p), mid, tr, l, r, cur);
-		}
-	}
-	void apply(int p){
-		if(!lazy[p].empty()){
-			tree[left(p)].apply(lazy[p]);
-			tree[right(p)].apply(lazy[p]);
-			
-			lazy[left(p)] += lazy[p];
-			lazy[right(p)] += lazy[p];
+            tree[p] = tree[left(p)] + tree[right(p)];
+        }
+    }
+    void query(int p, int tl, int tr, int l, int r, T& cur){
+        if(tl>=r || tr<=l){
+            return;
+        }else if(tl>=l && tr<=r){
+            cur += tree[p];
+        }else{
+            int mid = (tl + tr)/2;
+            apply(p);
+            query(left(p), tl, mid, l, r, cur);
+            query(right(p), mid, tr, l, r, cur);
+        }
+    }
+    void apply(int p){
+        if(!lazy[p].empty()){
+            tree[left(p)].apply(lazy[p]);
+            tree[right(p)].apply(lazy[p]);
 
-			lazy[p].reset();
-		}
-	}
+            lazy[left(p)] += lazy[p];
+            lazy[right(p)] += lazy[p];
+
+            lazy[p].reset();
+        }
+    }
 };
--- code/data-structures/segment-tree/standard.cpp
+++ code/data-structures/segment-tree/standard.cpp
@@ -25,41 +25,41 @@ public:
 		return cur;
 	}
 private:
-	int n;
-	vector<T>tree;
+    int n;
+    vector<T>tree;
     static int left (int p){ return 2 * p; }
     static int right (int p){ return 2 * p + 1; }
-	void build(int p, int tl, int tr, const vector<T>& arr){
-		if(tl + 1 == tr){
-			tree[p] = arr[tl];
-		}else{
-			int mid = (tl + tr)/2;
-			build(left(p), tl, mid, arr);
-			build(right(p), mid, tr, arr);
+    void build(int p, int tl, int tr, const vector<T>& arr){
+        if(tl + 1 == tr){
+            tree[p] = arr[tl];
+        }else{
+            int mid = (tl + tr)/2;
+            build(left(p), tl, mid, arr);
+            build(right(p), mid, tr, arr);
 
-			tree[p] = tree[left(p)] + tree[right(p)];
-		}
-	}
-	void update(int p, int tl, int tr, int x, const T& val){
-		if(tl + 1 == tr){
-			tree[p] = val;
-		}else{
-			int mid = (tl + tr)/2;
-			if(x<mid) update(left(p), tl, mid, x, val);
-			else update(right(p), mid, tr, x, val);
-	
-			tree[p] = tree[left(p)] + tree[right(p)];
-		}
-	}
-	void query(int p, int tl, int tr, int l, int r, T& cur){
-		if(tl>=r || tr<=l){
-			return;
-		}else if(tl>=l && tr<=r){
-			cur += tree[p];
-		}else{
-			int mid = (tl + tr)/2;
-			query(left(p), tl, mid, l, r, cur);
-			query(right(p), mid, tr, l, r, cur);
-		}
-	}
+            tree[p] = tree[left(p)] + tree[right(p)];
+        }
+    }
+    void update(int p, int tl, int tr, int x, const T& val){
+        if(tl + 1 == tr){
+            tree[p] = val;
+        }else{
+            int mid = (tl + tr)/2;
+            if(x<mid) update(left(p), tl, mid, x, val);
+            else update(right(p), mid, tr, x, val);
+
+            tree[p] = tree[left(p)] + tree[right(p)];
+        }
+    }
+    void query(int p, int tl, int tr, int l, int r, T& cur){
+        if(tl>=r || tr<=l){
+            return;
+        }else if(tl>=l && tr<=r){
+            cur += tree[p];
+        }else{
+            int mid = (tl + tr)/2;
+            query(left(p), tl, mid, l, r, cur);
+            query(right(p), mid, tr, l, r, cur);
+        }
+    }
 };
