374abe1928253522744275c45d808d7251b01261
Gabriel Meireles <gasm@cin.ufpe.br>
2021-11-07 22:45:10 -0300

    Fix indentation

--- code/data-structures/segment-tree/persistent.cpp
+++ code/data-structures/segment-tree/persistent.cpp
@@ -1,25 +1,25 @@
 struct Node{
-	int v = 0;
-	Node *l = this, *r = this;
+    int v = 0;
+    Node *l = this, *r = this;
 };
 const int ms = 1e5;	
 Node buffer[20 * ms]; // memory allocation for the nodes;
 Node* root[ms + 1]; // root[i] - pointer to the root of version i
 int cnt = 0;
 Node* update(Node* on, int tl, int tr, int x, int val){
-	Node* node = &buffer[cnt++];
-	*node = *on;
-	if(tl + 1 == tr){
-		node->v = val;
-		return node;
-	}else{
-		int mid = (tl + tr)/2;
-		if(x<mid) node->l = update(on->l, tl, mid, x,val);
-		else node->r = update(on->r, mid, tr, x, val);
-		
-		node->v = node->l->v + node->r->v;
-		return node;
-	}
+    Node* node = &buffer[cnt++];
+    *node = *on;
+    if(tl + 1 == tr){
+        node->v = val;
+        return node;
+    }else{
+        int mid = (tl + tr)/2;
+        if(x<mid) node->l = update(on->l, tl, mid, x,val);
+        else node->r = update(on->r, mid, tr, x, val);
+
+        node->v = node->l->v + node->r->v;
+        return node;
+    }
 }
 
 int query(Node* on,int tl, int tr, int l, int r){
