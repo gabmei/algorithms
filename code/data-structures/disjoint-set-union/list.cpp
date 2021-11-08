bbd3e7b72f5cb626b04ca1de5639c967bd5a067e
Gabriel Meireles <gasm@cin.ufpe.br>
2021-11-07 21:50:00 -0300

    refactor dsu folder

--- code/data-structures/disjoint-set-union/list.cpp
+++ code/data-structures/disjoint-set-union/list.cpp
@@ -1,80 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 struct DSU{
-  	vector<int>p;
-	vector<vector<int>>l;
-  	DSU(int n):p(n),l(n){//0-based
-	    for(int i=0;i<n;i++){
-	    	p[i]=i,l[i]={i};
-	    }
-	}
-	int getSet(int a){
-		return p[a];
-	}
-	void unionSet(int a, int b){
-		a=getSet(a);
-		b=getSet(b);
+    vector<int>p;
+    vector<vector<int>>l;
+    DSU(int n):p(n),l(n){
+        for(int i = 0; i < n; ++i){
+            p[i] = i;
+            l[i] = {i};
+        }
+    }
+    int root(int a){
+        return p[a];
+    }
+    bool unite(int a, int b){
+        a=root(a); b=root(b);
 
-		if(a!=b){
-			if(l[a].size()>l[b].size()){
-				swap(a,b);
-			}
-			while(!l[a].empty()){	
-				int u=l[a].back();
-				p[u]=b;
-
-				l[b].push_back(u);
-				l[a].pop_back();
-			}
-		}
-	}
+        if(a!=b){
+            //Union by size
+            if((int)l[a].size() < (int)l[b].size()) swap(a,b);
+            while(!l[b].empty()){
+                int u = l[b].back();
+                p[u] = a;
+                l[a].push_back(u);
+                l[b].pop_back();
+            }
+            return true;
+        }
+        return false;
+    }
 };
-
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n,m,k;
-	cin>>n>>m>>k;
-	DSU dsu(n);
-
-	for(int i=0;i<m;i++){
-		int u,v;
-		cin>>u>>v;
-	}
-
-	vector<vector<int>>foo;
-	for(int i=0;i<k;i++){
-		string a;
-		cin>>a;
-		int u,v;
-		cin>>u>>v;
-		u--;v--;
-		int tmp=a[0]=='c';
-		foo.push_back({tmp,u,v});
-	}
-	
-	reverse(foo.begin(),foo.end());
-
-	vector<string>ans;
-
-	for(auto& v:foo){
-		if(v[0]==1){//join
-			dsu.unionSet(v[1],v[2]);
-		}else{
-			if(dsu.getSet(v[1])==dsu.getSet(v[2])){
-				ans.push_back("YES");
-			}else{
-				ans.push_back("NO");
-			}
-		}
-		
-	}
-	
-	reverse(ans.begin(),ans.end());
-	for(string& s:ans){
-		cout<<s<<'\n';
-	}
-	
-	return 0;
-}
--- code/data-structures/disjoint-set-union/standard.cpp
+++ code/data-structures/disjoint-set-union/standard.cpp
@@ -1,33 +1,34 @@
 struct DSU{
-    vector<int>p,sz;
-    DSU(int n):p(n),sz(n){//0-based
-        iota(p.begin(),p.end(),0);
-        fill(sz.begin(),sz.end(),1);
+    vector<int>p, sz;
+    DSU(int n):p(n),sz(n){
+        iota(begin(p), end(p), 0);
+        fill(begin(sz), end(sz), 1);
     }
     int root(int a){
-        return p[a]=(p[a]==a?a:root(p[a]));//path compression
+        return p[a] = (p[a] == a ? a : root(p[a]));//path compression
     }
-    void unite(int a, int b){
-        a=root(a);
-        b=root(b);
- 
+    bool unite(int a, int b){
+        a=root(a); b=root(b);
+
         if(a!=b){
             //Union by size
-            if(sz[a]<sz[b]){
-                swap(a,b);
-            }
-            p[b]=a;
-            sz[a]+=sz[b];
+            if(sz[a] < sz[b]) swap(a,b);
+            
+            p[b] = a;
+            sz[a] += sz[b];
+
+            return true;
         }
+        return false;
     }
     int size(int x){
         return sz[root(x)];
     }
     int count(){
-    	int n = (int)p.size(),cnt=0;
-    	for(int i=0;i<n;i++){
-    		cnt+=i==root(i) ? 1 : 0;
-    	}
-    	return cnt;
+        int n = (int)p.size(), cnt = 0;
+        for(int i = 0; i < n; ++i){
+            cnt += i == root(i) ? 1 : 0;
+        }
+        return cnt;
     }
 };
