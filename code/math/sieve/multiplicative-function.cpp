f1e81e0c10efa68ce1ed6df918019c3b71b41169
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-11-15 12:35:32 -0300

    Update multiplicative-function.cpp

--- code/math/sieve/multiplicative-function.cpp
+++ code/math/sieve/multiplicative-function.cpp
@@ -4,18 +4,17 @@ struct MultiplicativeFunction{
     vector<T> ans;
     vector<bool> pr;
     MultiplicativeFunction(int n):ans(n, 1), pr(n, true){
+        pr[1] = false;
         for(int i = 2; i < n; ++i){
             if(!pr[i]) continue;
-            
-            for(int u = i, e = 1; true; u *= i, ++e){
-                ans[u] = f(i, e);
-                if(n / u / i == 0)break; //checks for overflow
+            ans[i] = f(i, 1);
+            for(int u = i, e = 2; u < n / i; u *= i, ++e){
+                ans[u * i] = f(i, e);
             }
-
             for(int j = i + i; j < n; j += i){
                 int x = j;
                 while(x % i == 0) x /= i;
-                ans[j] = ans[x] * ans[j / x]; // multiplicative function
+                ans[j] = ans[x] * ans[j / x]; // multiplicative property: (x, j / x) = 1 
                 pr[j] = false;
             }
         }
@@ -23,9 +22,9 @@ struct MultiplicativeFunction{
     const T& operator[](int i){ return ans[i]; }
 };
 template<class T>
-T mobiusFunction(int p, int e){return e > 1 ? 0 : -1; }
+T mobiusFunction(int, int e){ return e > 1 ? 0 : -1; }
 template<class T>
-using Mobius = MultiplicativeFunction<T,mobiusFunction<T>>;
+using Mobius = MultiplicativeFunction<T, mobiusFunction<T>>;
 
 template<class T>
 T eulerTotient(int p, int e){
@@ -39,7 +38,7 @@ using PHI = MultiplicativeFunction<T, eulerTotient<T>>;
 template<class T>
 T numberOfDivisors(int, int e){ return e + 1; }
 template<class T>
-using NUMDIV = MultiplicativeFunction<T,numberOfDivisors<T>>;
+using NUMDIV = MultiplicativeFunction<T, numberOfDivisors<T>>;
 
 template<class T>
 T sumOfDivisors(int p, int e){
@@ -48,4 +47,4 @@ T sumOfDivisors(int p, int e){
     return (pw - 1) / (p - 1);
 }
 template<class T>
-using SUMDIV = MultiplicativeFunction<T,sumOfDivisors<T>>;
+using SUMDIV = MultiplicativeFunction<T, sumOfDivisors<T>>;
