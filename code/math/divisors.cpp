f8c569d17ae8c37158a372d6d7a29edc770207eb
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2021-11-14 23:32:55 -0300

    Update divisors.cpp

--- code/math/divisors.cpp
+++ code/math/divisors.cpp
@@ -1,9 +1,9 @@
 template<class T>
-void getDivisors(int x, T get){
+void getDivisors(int x, T&& get){
     for(int i=1;i*i<=x;++i){
         if(x%i==0){
             get(i);
-            get(x/i);
+            if(x/i != i)get(x/i);
         }
     }
     return;
