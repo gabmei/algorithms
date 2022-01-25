425d5fa2f1d5515f9aaab2bc321e759e77e7d009
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2022-01-25 09:22:10 -0300

    Update rabin-karp.cpp

--- code/string/rabin-karp.cpp
+++ code/string/rabin-karp.cpp
@@ -1,15 +1,21 @@
 template<class F = unsigned int, class T = string>
 struct RabinKarp{
     int n;
-    vector< F > p, pw;
+    vector< F > p[2], pw[2];
     static constexpr F C[2] = {727, 137};
     RabinKarp(){}
-    //change C id for double hashing
-    RabinKarp(const T& s, bool id):n((int)s.size()),p(n+1),pw(n+1,1){
-        for(int i=1;i<=n;i++){
-            pw[i] = pw[i-1]*C[id];
-            p[i] = p[i-1]*C[id] + s[i-1];
+    RabinKarp(const T& s):n((int)s.size()){
+        for(int e = 0; e < 2; ++e){
+            //p(n+1),pw(n+1,1)
+            p[e].assign(n + 1, 0);
+            pw[e].assign(n + 1, 1);
+            for(int i = 1; i <= n; ++i){
+                pw[e][i] = pw[e][i-1]*C[e];
+                p[e][i] = p[e][i-1]*C[e] + s[i-1];
+            }
         }
     }
-    F hash(int i, int len){return p[i+len] - pw[len]*p[i];}
+    pair<F, F> hash(int i, int len){
+        return {p[0][i+len] - pw[0][len]*p[0][i], p[1][i+len] - pw[1][len]*p[1][i]};
+    }
 };
