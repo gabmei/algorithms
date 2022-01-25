2cf2299983009971b63d3040f7a8a348ab8692c6
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2022-01-25 17:46:56 -0300

    Update rabin-karp.cpp

--- code/string/rabin-karp.cpp
+++ code/string/rabin-karp.cpp
@@ -6,16 +6,15 @@ struct RabinKarp{
     RabinKarp(){}
     RabinKarp(const T& s):n((int)s.size()){
         for(int e = 0; e < 2; ++e){
-            //p(n+1),pw(n+1,1)
             p[e].assign(n + 1, 0);
             pw[e].assign(n + 1, 1);
             for(int i = 1; i <= n; ++i){
-                pw[e][i] = pw[e][i-1]*C[e];
-                p[e][i] = p[e][i-1]*C[e] + s[i-1];
+                pw[e][i] = pw[e][i - 1] * C[e];
+                p[e][i] = p[e][i - 1] * C[e] + s[i - 1];
             }
         }
     }
     pair<F, F> hash(int i, int len){
-        return {p[0][i+len] - pw[0][len]*p[0][i], p[1][i+len] - pw[1][len]*p[1][i]};
+        return {p[0][i + len] - pw[0][len] * p[0][i], p[1][i + len] - pw[1][len] * p[1][i]};
     }
 };
