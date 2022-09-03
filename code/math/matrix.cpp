e270483d990e3c6533a0eb1477b8633867a96f0f
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2022-09-03 14:18:48 -0300

    Update matrix.cpp

--- code/math/matrix.cpp
+++ code/math/matrix.cpp
@@ -1,16 +1,21 @@
 const int N = 2;
 template<class T>
 struct Matrix {
-	array<array<T, N>, N> a;
-	Matrix(bool identity=false) {
-		for(int i = 0; i < N; ++i) a[i][i] = T(identity);
-	}
-	Matrix operator *(const Matrix& b) {
-		Matrix p;
-		for(int i = 0; i < N; ++i)
-			for(int j = 0; j < N; ++j)
-				for(int k = 0; k < N; ++k)
-					p.a[i][k] += a[i][j] * b.a[j][k];
-		return p;
-	}
+    T a[N][N];
+    Matrix(bool identity = false) {
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                a[i][j] = 0;
+            }
+            a[i][i] = T(identity);
+        }
+    }
+    Matrix operator *(const Matrix& b) {
+        Matrix p;
+        for(int i = 0; i < N; ++i)
+            for(int j = 0; j < N; ++j)
+                for(int k = 0; k < N; ++k)
+                    p.a[i][k] += a[i][j] * b.a[j][k];
+        return p;
+    }
 };
