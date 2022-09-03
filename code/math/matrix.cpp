544db2c2f131decc7956fc0ba38e4d0b59472b42
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2022-09-03 14:28:06 -0300

    Update matrix.cpp

--- code/math/matrix.cpp
+++ code/math/matrix.cpp
@@ -5,7 +5,7 @@ struct Matrix {
     Matrix(bool identity = false) {
         for(int i = 0; i < N; ++i) {
             for(int j = 0; j < N; ++j) {
-                a[i][j] = 0;
+                a[i][j] = T(0);
             }
             a[i][i] = T(identity);
         }
