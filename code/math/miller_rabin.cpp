299b780ac2240bda5cf714f3fe9877bd0c8c5a43
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-01 16:54:34 -0300

    Update miller_rabin.cpp

--- code/math/miller_rabin.cpp
+++ code/math/miller_rabin.cpp
@@ -1,58 +1,58 @@
 // https://github.com/kth-competitive-programming/kactl/blob/main/content/number-theory/Factor.h
 ull modmul(ull a, ull b, ull M) {
-	ll ret = a * b - M * ull(1.L / M * a * b);
-	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+  ll ret = a * b - M * ull(1.L / M * a * b);
+  return ret + M * (ret < 0) - M * (ret >= (ll)M);
 }
 ull modpow(ull b, ull e, ull mod) {
-	ull ans = 1;
-	for (; e; b = modmul(b, b, mod), e /= 2) {
-		if (e & 1) {
-			ans = modmul(ans, b, mod);
-		}
-	}
-	return ans;
+  ull ans = 1;
+  for (; e; b = modmul(b, b, mod), e /= 2) {
+    if (e & 1) {
+      ans = modmul(ans, b, mod);
+    }
+  }
+  return ans;
 }
 bool is_prime(ull n) {
-	if (n < 2 || n % 6 % 4 != 1) {
-		return (n | 1) == 3;
-	}
-	ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
-	    s = __builtin_ctzll(n - 1), d = n >> s;
-	for (ull a : A) {   // ^ count trailing zeroes
-		ull p = modpow(a % n, d, n), i = s;
-		while (p != 1 && p != n - 1 && a % n && i--) {
-			p = modmul(p, p, n);
-		}
-		if (p != n-1 && i != s) {
-			return 0;
-		}
-	}
-	return 1;
+  if (n < 2 || n % 6 % 4 != 1) {
+    return (n | 1) == 3;
+  }
+  ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+  ull s = __builtin_ctzll(n - 1), d = n >> s;
+  for (ull a : A) {   // ^ count trailing zeroes
+    ull p = modpow(a % n, d, n), i = s;
+    while (p != 1 && p != n - 1 && a % n && i--) {
+      p = modmul(p, p, n);
+    }
+    if (p != n-1 && i != s) {
+      return 0;
+    }
+  }
+  return 1;
 }
-
+ 
 ull pollard(ull n) {
-	auto f = [n](ull x) { return modmul(x, x, n) + 1; };
-	ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
-	while (t++ % 40 || gcd(prd, n) == 1) {
-		if (x == y) {
+  auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+  ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+  while (t++ % 40 || gcd(prd, n) == 1) {
+    if (x == y) {
       x = ++i, y = f(x);
     }
-		if ((q = modmul(prd, max(x,y) - min(x,y), n))) {
+    if ((q = modmul(prd, max(x,y) - min(x,y), n))) {
       prd = q;
     }
-		x = f(x), y = f(f(y));
-	}
-	return gcd(prd, n);
+    x = f(x), y = f(f(y));
+  }
+  return gcd(prd, n);
 }
 vector<ull> factor(ull n) {
-	if (n == 1) {
+  if (n == 1) {
     return {};
   }
-	if (isPrime(n)) {
+  if (is_prime(n)) {
     return {n};
   }
-	ull x = pollard(n);
-	auto l = factor(x), r = factor(n / x);
-	l.insert(end(l), begin(r), end(r));
-	return l;
+  ull x = pollard(n);
+  auto l = factor(x), r = factor(n / x);
+  l.insert(end(l), begin(r), end(r));
+  return l;
 }
