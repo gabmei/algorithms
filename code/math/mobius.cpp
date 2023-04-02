0c4c9a32b602c88e73cd9cb4d65349e849e9c8d0
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-02 18:13:06 -0300

    Update mobius.cpp

--- code/math/mobius.cpp
+++ code/math/mobius.cpp
@@ -1,27 +1,11 @@
 //https://www.quora.com/q/suryakiransposts/A-Dance-with-Mobius-Function
-typedef long long ll;
-const int LIM=1e6+100;
-bool is_prime[LIM];
+const int LIM = 1e6 + 10;
 int mu[LIM];
 void sieve() {
-  for(int i = 1; i < LIM; ++i) {
-    is_prime[i] = true;
-    mu[i] = 1;
-  }
-  is_prime[1] = is_prime[0] = false;
-  for(int i = 2; i < LIM; ++i) {
-    if(is_prime[i]) {
-      mu[i] = -1;
-      for(int j = 2 * i; j < LIM; j += i) {
-        mu[j] *= -1;
-        is_prime[j] =false;
-      }
-      //LIM <= i * i
-      if(LIM / i / i == 0)continue;
-      int sq = i * i;
-      for(int j = sq; j < LIM; j += sq) {
-        mu[j] = 0;
-      }
+  mu[1] = 1;
+  for(int i = 1; i < MAXN; ++i) {
+    for(int j = 2 * i; j < MAXN; j += i) {
+      mu[j] -= mu[i];
     }
   }
 }
