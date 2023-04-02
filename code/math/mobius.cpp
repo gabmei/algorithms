90b4a200d3028823e0ec51b54eb415c8a3528849
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-02 18:42:00 -0300

    Update mobius.cpp

--- code/math/mobius.cpp
+++ code/math/mobius.cpp
@@ -1,11 +1,20 @@
 //https://www.quora.com/q/suryakiransposts/A-Dance-with-Mobius-Function
 const int LIM = 1e6 + 10;
 int mu[LIM];
+vector<int> mask[LIM];
+
 void sieve() {
   mu[1] = 1;
   for(int i = 1; i < LIM; ++i) {
     for(int j = 2 * i; j < LIM; j += i) {
       mu[j] -= mu[i];
+    }
+  }
+  for(int i = 2; i < LIM; ++i) {
+    if(mu[i] != 0) {
+      for(int j = i; j < LIM; j += i) {
+        mask[j].emplace_back(i);
+      }
     }
   }
 }
