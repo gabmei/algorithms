96e4a11a8f917e0a5c23496a6e804fbba0577400
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-02 18:13:22 -0300

    Update mobius.cpp

--- code/math/mobius.cpp
+++ code/math/mobius.cpp
@@ -3,8 +3,8 @@ const int LIM = 1e6 + 10;
 int mu[LIM];
 void sieve() {
   mu[1] = 1;
-  for(int i = 1; i < MAXN; ++i) {
-    for(int j = 2 * i; j < MAXN; j += i) {
+  for(int i = 1; i < LIM; ++i) {
+    for(int j = 2 * i; j < LIM; j += i) {
       mu[j] -= mu[i];
     }
   }
