f60ce3a30333151fc47b9b4f9732257df6f42e15
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-07 22:37:01 -0300

    Update modular_arithmetic.cpp

--- code/math/modular_arithmetic.cpp
+++ code/math/modular_arithmetic.cpp
@@ -1,42 +1,47 @@
-const int MOD = 1e9 + 7;
 template<typename T>
-T bin_exp(T a, long long e){
+T bin_exp(T a, long long e) {
   T r(1);
-  for(; e > 0; e >>= 1){
-    if(e & 1) r *= a;
+  for(; e > 0; e >>= 1) {
+    if(e & 1) {
+      r *= a;
+    }
     a *= a;
   }
   return r;
 }
-struct MB {
-  int x;
-  MB(int x = 0) : x(x < 0 ? x + MOD : x >= MOD ? x - MOD : x) {}
-  MB& operator+=(const MB& rhs) {
+template<const uint32_t MOD>
+struct Mod {
+  uint32_t x;
+  Mod() : x(0) {};
+  template<typename T>
+  Mod(T x) : x(uint32_t(((int64_t(x) % MOD) + MOD) % MOD)) {}
+  Mod& operator+=(Mod rhs) {
     x += rhs.x;
     if(x >= MOD) x -= MOD;
     return *this;
   }
-  MB& operator-=(const MB& rhs) {
-    x -= rhs.x;
-    if(x < 0) x += MOD;
-    return *this; 
-  }
-  MB& operator*=(const MB& rhs) {
-    long long y = 1ll * x * rhs.x;
-    if(y >= MOD) y %= MOD;
-    x = int(y);
+  Mod& operator-=(Mod rhs) {
+    x += MOD - rhs.x;
+    if(x >= MOD) x -= MOD;
     return *this;
   }
-  MB& operator/=(const MB& rhs) {
-    return *this *= bin_exp(rhs, MOD - 2);
+  Mod& operator*=(Mod rhs) {
+    auto y = 1ull * x * rhs.x;
+    if(y >= MOD) y %= MOD;
+    x = uint32_t(y);
+    return *this;
   }
-  friend MB operator+(MB lhs, const MB& rhs) { return lhs += rhs; }
-  friend MB operator-(MB lhs, const MB& rhs) { return lhs -= rhs; }
-  friend MB operator*(MB lhs, const MB& rhs) { return lhs *= rhs; }
-  friend MB operator/(MB lhs, const MB& rhs) { return lhs /= rhs; }
-  MB operator-() const { return MB() - *this; }
-  bool operator==(const MB& rhs) const { return x == rhs.x; }
-  bool operator!=(const MB& rhs) const { return !(*this == rhs); }
-  friend ostream& operator<< (ostream& os, const MB& o) { return os << o.x; }
-  friend istream& operator>> (istream& is, MB& o) { return is >> o.x; }
+  Mod& operator/=(Mod rhs) { return *this *= bin_exp(rhs, MOD - 2); }
+  friend Mod operator+(Mod lhs, Mod rhs) { return lhs += rhs; }
+  friend Mod operator-(Mod lhs, Mod rhs) { return lhs -= rhs; }
+  friend Mod operator*(Mod lhs, Mod rhs) { return lhs *= rhs; }
+  friend Mod operator/(Mod lhs, Mod rhs) { return lhs /= rhs; }
+  bool operator==(Mod rhs) const { return x == rhs.x; }
+  friend ostream& operator<<(ostream& os, const Mod& o) { return os << o.x; }
+  friend istream& operator>>(istream& is, Mod& o) { 
+    int64_t x;
+    is >> x;
+    o = Mod(x);
+    return is;
+  }
 };
