3b4537de6b201726313b2fb7f5ecab221363e9d5
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-04-07 22:34:40 -0300

    Update rabin_karp.cpp

--- code/string/rabin_karp.cpp
+++ code/string/rabin_karp.cpp
@@ -1,33 +1,42 @@
-const MB C[2] = {727, 137}; // double hashing
-template<typename T = string>
+template<typename Mint>
 struct RabinKarp {
   int n;
-  vector<MB> p[2], pw[2];
-  RabinKarp(const T& s = {}) : n(int(s.size())) {
-    for(int e = 0; e < 2; ++e) {
-      p[e].assign(n + 1, 0);
-      pw[e].assign(n + 1, 1);
-      for(int i = 1; i <= n; ++i) {
-        pw[e][i] = pw[e][i - 1] * C[e];
-        p[e][i] = p[e][i - 1] * C[e] + s[i - 1];
-      }
+  vector<Mint> p, pw;
+  RabinKarp() {}
+  template<typename T>
+  RabinKarp(const T& s, Mint C) : n(int(s.size())) {
+    pw.assign(n + 1, 1);
+    p.assign(n + 1, 0);
+    for(int i = 1; i <= n; ++i) {
+      pw[i] = pw[i - 1] * C;
+      p[i] = p[i - 1] * C + s[i - 1];
     }
   }
-  pair<int, int> hash(int i, int len) const {
-    int h0 = (p[0][i + len] - pw[0][len] * p[0][i]).x;
-    int h1 = (p[1][i + len] - pw[1][len] * p[1][i]).x;
-    return {h0, h1};
+  Mint hash(int i, int len) const {
+    return (p[i + len] - pw[len] * p[i]);
   }
 };
-template<typename T>
+template<typename Mint>
 struct Hash {
-  RabinKarp<T> rab, rev_rab;
-  Hash(const T& s) {
+  int n;
+  RabinKarp<Mint> rab[2], rev_rab[2];
+  template<typename T>
+  Hash(const T& s, Mint C0 = 727, Mint C1 = 137) : n((int)s.size()) {
+    Mint C[2] = {C0, C1};
     auto rev_s = s;
     reverse(begin(rev_s), end(rev_s));
-    rab = RabinKarp<T>(s);
-    rev_rab = RabinKarp<T>(rev_s);
+    for(int e = 0; e < 2; ++e) {
+      rab[e] = RabinKarp<Mint>(s, C[e]);
+      rev_rab[e] = RabinKarp<Mint>(rev_s, C[e]);
+    }
   }
-  pair<int, int> getHash(int l, int r) const { return rab.hash(l, r - l + 1); }
-  pair<int, int> getReverseHash(int l, int r) const { return rev_rab.hash(rev_rab.n - r - 1, r - l + 1); }
+  pair<Mint, Mint> get_hash(int l, int r) const {
+    return {rab[0].hash(l, r - l), rab[1].hash(l, r - l)};
+  }
+  pair<Mint, Mint> get_reverse_hash(int l, int r) const {
+    return {rev_rab[0].hash(n - r, r - l), rev_rab[1].hash(n - r, r - l)};
+  }
+  bool is_palindrome(int l, int r) const {
+    return get_hash(l, r) == get_reverse_hash(l, r);
+  }
 };
