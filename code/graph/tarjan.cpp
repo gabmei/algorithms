e09910689adfea32e31a6e15c2a2869da102a05b
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2023-07-12 23:10:34 -0300

    Update tarjan.cpp

--- code/graph/tarjan.cpp
+++ code/graph/tarjan.cpp
@@ -1,31 +1,28 @@
-// thanks to: https://github.com/celiopassos/competitive-programming/blob/master/Graphs/tarjan.cpp
-// scc ids are sorted in reverse order
-// edge uv imples scc[u] >= scc[v]
-// it is garanteed that 0 <= scc[u] < n for all u
 vector<int> tarjan(const vector<vector<int>>& adj) {
   int n = (int)adj.size(), timer = 0, ncomps = 0;
   enum State { unvisited, on_stack, visited };
   vector<State> state(n, unvisited);
-  vector<int> low(n), id(n), scc(n), stk;
+  vector<int> low(n), tin(n), scc(n), stk;
   auto dfs = [&](auto&& dfs, int u) -> void {
-    low[u] = id[u] = timer++;
+    low[u] = tin[u] = timer++;
     stk.push_back(u);
     state[u] = on_stack;
     for(int v : adj[u]) {
       if(state[v] == unvisited) {
         dfs(dfs, v);
-      }
-      if(state[v] == on_stack) {
         low[u] = min(low[u], low[v]);
+      } else if(state[v] == on_stack) {
+        low[u] = min(low[u], tin[v]);
       }
     }
-    if(low[u] == id[u]) {
+    if(low[u] == tin[u]) {
+      int v;
       do {
-        int v = stk.back();
+        v = stk.back();
         stk.pop_back();
         state[v] = visited;
         scc[v] = ncomps;
-      } while(!stk.empty() && id[stk.back()] >= id[u]);
+      } while(v != u);
       ++ncomps;
     }
   };
