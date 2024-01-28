f9982f3dc4295ee8aa0d3ad92a81fc2b29b8d56a
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2024-01-28 18:48:53 -0300

    Update kosaraju.cpp

--- code/graph/kosaraju.cpp
+++ code/graph/kosaraju.cpp
@@ -5,17 +5,16 @@ vector<int> kosaraju(const vector<vector<int>>& adj) {
     for(auto v : adj[u]) radj[v].emplace_back(u);
   }
   vector<int> ord;
-  vector<int> tin(n, -1);
-  int t = 0;
+  vector<bool> vis(n);
   auto dfs = [&](auto&& dfs, int u) -> void {
-    tin[u] = t++;
+    vis[u] = true;
     for(auto v : adj[u]) {
-      if(tin[v] == -1) dfs(dfs, v);
+      if(!vis[v]) dfs(dfs, v);
     }
     ord.emplace_back(u);
   };
   for(int u = 0; u < n; ++u) {
-    if(tin[u] == -1) dfs(dfs, u);
+    if(!vis[u]) dfs(dfs, u);
   }
   vector<int> scc(n, -1);
   int comp = 0;
