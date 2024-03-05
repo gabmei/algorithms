25c8acdffd76739cd27c88d5e9967402e3e46a53
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2024-03-05 17:11:42 -0300

    Update aho_corasick.cpp

--- code/string/aho_corasick.cpp
+++ code/string/aho_corasick.cpp
@@ -4,11 +4,11 @@ struct AhoType {
 };
 template<typename AhoType>
 struct AhoCorasick {
-  static const AhoType T;
   struct Node {
-    int nxt[T.ALPHA] {};
+    int nxt[AhoType::ALPHA] {};
     int p = 0, ch = 0, len = 0;
-    int occ_link = 0, link = 0;  
+    int link = 0;
+    int occ_link = 0;
     Node(int p = 0, int ch = 0, int len = 0) : p(p), ch(ch), len(len) {}
   };
   vector<Node> tr;
@@ -17,7 +17,7 @@ struct AhoCorasick {
   void add_word(Iterator first, Iterator last) {
     int cur = 0, len = 1;
     for(; first != last; ++first) {
-      auto ch = T.f(*first);
+      auto ch = AhoType::f(*first);
       if(tr[cur].nxt[ch] == 0) {
         tr[cur].nxt[ch] = int(tr.size());
         tr.emplace_back(cur, ch, len);
@@ -35,7 +35,7 @@ struct AhoCorasick {
       if(tr[v].occ_link == 0) {
         tr[v].occ_link = tr[u].occ_link;
       }
-      for(int ch = 0; ch < T.ALPHA; ++ch) {
+      for(int ch = 0; ch < AhoType::ALPHA; ++ch) {
         auto& nxt = tr[v].nxt[ch];
         if(nxt == 0) {
           nxt = tr[u].nxt[ch];
@@ -49,11 +49,13 @@ struct AhoCorasick {
   template<typename Iterator, typename Report>
   void get_all_matches(Iterator first, Iterator last, Report&& report) const {
     for(int cur = 0, i = 0; first != last; ++i, ++first) {
-      auto ch = T.f(*first);
+      auto ch = AhoType::f(*first);
       cur = tr[cur].nxt[ch];
       for(int v = tr[cur].occ_link; v > 0; v = tr[tr[v].link].occ_link) {
         report(i, v);
       }
     }
   }
+  template<typename T>
+  int get_next(int cur, T ch) const { return tr[cur].nxt[AhoType::f(ch)]; }
 };
