7366256c31d33e093e48ec86de9c06d888b4d8da
Gabriel Meireles <48731050+gabmei@users.noreply.github.com>
2024-08-02 19:45:36 -0300

    Update centroid_decomposition.cpp

--- code/graph/centroid_decomposition.cpp
+++ code/graph/centroid_decomposition.cpp
@@ -1,14 +1,14 @@
 vector<int> adj[MAXN]; int sz[MAXN]; bool blocked[MAXN];
 int preprocess(int u, int p) {
   sz[u] = 1;
-  for(int v : adj[u]) {
+  for(auto v : adj[u]) {
     if(v == p || blocked[v]) continue;
     sz[u] += preprocess(v, u);
   }
   return sz[u];
 }
 int get_centroid(int u, int p, int tree_size) {
-  for(int v : adj[u]) {
+  for(auto v : adj[u]) {
     if(v == p || blocked[v]) continue;
     if(2 * sz[v] >= tree_size) return get_centroid(v, u, tree_size);
   }
